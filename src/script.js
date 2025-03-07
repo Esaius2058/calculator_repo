c3a92d9239b28e80055c5825960e08ed8435bab3
Esaius2058 <wabwirewild@gmail.com>
2025-03-07 17:55:44 +0300

    operate function

---
+++ src/script.js
@@ -0,0 +1,33 @@
+function operate(operation, num1, num2){
+    if(operation == "add"){
+        return add(num1, num2);
+    }
+    else if(operation == "subtract"){
+        return subtract(num1, num2);
+    }
+    else if(operation == "multiply"){
+        return multiply(num1, num2);
+    }
+    else if(operation == "divide"){
+        return divide(num1, num2);
+    }
+
+    /*var operation = document.getElementById("operation").value;
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = 0;
+    if(operation == "add"){
+        result = add(num1, num2);
+    }
+    else if(operation == "subtract"){
+        result = subtract(num1, num2);
+    }
+    else if(operation == "multiply"){
+        result = multiply(num1, num2);
+    }
+    else if(operation == "divide"){
+        result = divide(num1, num2);
+    }
+    document.getElementById("result").innerHTML = result;  
+    */
+}
