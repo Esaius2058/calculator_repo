86fbfd66ee1977598ddb579f5e7daa203f3041fc
Lee Kaburu <leekaburu@gmail.com>
2025-03-07 18:24:19 +0300

    subtract function
    multiply function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/*operator = document.getElementById('operator');
+num1 = document.getElementById('num1');
+num2 = document.getElementById('num2');*/
+
+var num1, num2;
 
 function subtract(num1, num2) {
     return num1 - num2;
